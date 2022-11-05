8ffc573041545c3633c59d4de54f7cdc13698ab3
Sofia <sofiarodrigues45@gmail.com>
2022-11-05 11:26:26 -0300

    adicionando redirecionamento pós animação na splash screen

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 import BrandSvg from '../../assets/brand.svg'
 import LogoSvg from '../../assets/logo.svg'
@@ -8,13 +9,16 @@ import Animated, {
     useSharedValue, 
     withTiming,
     interpolate,
-    Extrapolate
+    Extrapolate,
+    runOnJS
 } from 'react-native-reanimated';
 
 import { Container } from './styles';
 
 export function Splash() {
     const splashAnimation = useSharedValue(0)
+
+    const navigation = useNavigation()
 
     const brandStyle = useAnimatedStyle(() => {
         return {
@@ -46,10 +50,18 @@ export function Splash() {
         }
     })
 
+    function startApp() {
+        navigation.navigate('Home')
+    }
+
     useEffect(() => {
         splashAnimation.value = withTiming(
             50, 
-            { duration: 1000 }
+            { duration: 1500 },
+            () => {
+                'worklet'
+                runOnJS(startApp)()
+            }
         )
     }, [])
 
