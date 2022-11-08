1addf9ae46f8e4e50da0c674f3565c24c45a9dd2
Sofia <sofiarodrigues45@gmail.com>
2022-11-08 20:59:13 -0300

    adicionando acessibilidade ao formulário de login

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { useTheme } from 'styled-components';
 
 import { Button } from '../../components/Button';
@@ -19,36 +19,38 @@ export function SignIn() {
   const theme = useTheme()
 
   return (
-    <Container>
-      <StatusBar 
-        barStyle='dark-content'
-        backgroundColor="transparent"
-        translucent
-      />
-      <Header>
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+        <StatusBar 
+          barStyle='dark-content'
+          backgroundColor="transparent"
+          translucent
+        />
+        <Header>
         <Title>Estamos {'\n'}quase lá.</Title>
         <SubTitle>
           Faça seu login para começar {'\n'}
           uma experiência incrível.
         </SubTitle>
-      </Header>
+        </Header>
 
-      <Form>
-        <Input 
-          iconName="mail"
-          placeholder="E-mail"
-          keyboardType="email-address"
-          autoCorrect={false}
-          autoCapitalize="none"
-        />
+        <Form>
+          <Input 
+            iconName="mail"
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
 
-        <PasswordInput 
-          iconName="lock"
-          placeholder="Senha"
-        />
-      </Form>
+          <PasswordInput 
+            iconName="lock"
+            placeholder="Senha"
+          />
+        </Form>
 
-      <Footer>
+        <Footer>
         <Button
           title="Login"
           onPress={() => {}}
@@ -64,7 +66,9 @@ export function SignIn() {
           loading={false}
           light
         />
-      </Footer>
-    </Container>
+        </Footer>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
