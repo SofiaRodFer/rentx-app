c35c3529f43c18833a924f9e8631519d4ae4696c
Sofia <sofiarodrigues45@gmail.com>
2022-11-08 21:42:21 -0300

    adicionando validação de formulário

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { StatusBar, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import * as Yup from 'yup'
+import { StatusBar, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
 import { useTheme } from 'styled-components';
 
 import { Button } from '../../components/Button';
@@ -20,6 +21,29 @@ export function SignIn() {
   const [password, setPassword] = useState('')
 
   const theme = useTheme()
+
+  async function handleSignIn() {
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string()
+          .required('A senha é obrigatória')
+      })
+  
+      await schema.validate({ email, password })
+    } catch (error) {
+      if(error instanceof Yup.ValidationError) {
+        return Alert.alert('Opa', error.message)
+      } else {
+        Alert.alert(
+          'Erro na autenticação', 
+          'Ocorreu um erro ao fazer login, verifique as credenciais.'
+        )
+      }
+    }
+  }
 
   return (
     <KeyboardAvoidingView behavior="position" enabled>
@@ -60,7 +84,7 @@ export function SignIn() {
         <Footer>
         <Button
           title="Login"
-          onPress={() => {}}
+          onPress={handleSignIn}
           enabled={false}
           loading={false}
         />
