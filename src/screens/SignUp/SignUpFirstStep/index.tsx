f7acd85bea14946547222f5307f818b025f6f2aa
Sofia <sofiarodrigues45@gmail.com>
2022-11-09 16:41:38 -0300

    criando telas de criação de conta

--- src/screens/SignUp/SignUpFirstStep/index.tsx
+++ src/screens/SignUp/SignUpFirstStep/index.tsx
@@ -24,42 +24,42 @@ export function SignUpFirstStep() {
     <KeyboardAvoidingView behavior="position" enabled>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <Container>
-      <Header>
-        <BackButton onPress={handleBack} />
-
-        <Steps>
-          <Bullet active />
-          <Bullet />
-        </Steps>
-      </Header>
-
-      <Title>Crie sua {'\n'}conta</Title>
-      <Subtitle>Faça seu cadastro de {'\n'}forma rápida e fácil</Subtitle>
-
-      <Form>
-        <FormTitle>1. Dados</FormTitle>
-        <Input 
-          iconName="user"
-          placeholder="Nome"
-        />
-
-        <Input 
-          iconName="mail"
-          placeholder="E-mail"
-          keyboardType="email-address"
-        />
-
-        <Input 
-          iconName="credit-card"
-          placeholder="CNH"
-          keyboardType="numeric"
-        />
-      </Form>
-
-      <Button 
-        title="Próximo"
-        onPress={handleNextStep}
-      />
+          <Header>
+            <BackButton onPress={handleBack} />
+      
+            <Steps>
+              <Bullet active />
+              <Bullet />
+            </Steps>
+          </Header>
+      
+          <Title>Crie sua {'\n'}conta</Title>
+          <Subtitle>Faça seu cadastro de {'\n'}forma rápida e fácil</Subtitle>
+      
+          <Form>
+            <FormTitle>1. Dados</FormTitle>
+            <Input 
+              iconName="user"
+              placeholder="Nome"
+            />
+    
+            <Input 
+              iconName="mail"
+              placeholder="E-mail"
+              keyboardType="email-address"
+            />
+    
+            <Input 
+              iconName="credit-card"
+              placeholder="CNH"
+              keyboardType="numeric"
+            />
+          </Form>
+      
+          <Button 
+            title="Próximo"
+            onPress={handleNextStep}
+          />
         </Container>
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
--- src/screens/SignUp/SignUpSecondStep/index.tsx
+++ src/screens/SignUp/SignUpSecondStep/index.tsx
@@ -1,15 +1,17 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from 'react-native'
+import { useTheme } from 'styled-components';
 
 import { BackButton } from '../../../components/BackButton';
 import { Bullet } from '../../../components/Bullet';
-import { Input } from '../../../components/Input';
 import { Button } from '../../../components/Button';
 
 import { Container, Header, Steps, Title, Subtitle, Form, FormTitle } from './styles';
+import { PasswordInput } from '../../../components/PasswordInput';
 
 export function SignUpSecondStep() {
+  const theme = useTheme()
   const navigation = useNavigation()
 
   function handleBack() {
@@ -20,26 +22,36 @@ export function SignUpSecondStep() {
     <KeyboardAvoidingView behavior="position" enabled>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <Container>
-      <Header>
-        <BackButton onPress={handleBack} />
+          <Header>
+            <BackButton onPress={handleBack} />
 
-        <Steps>
-          <Bullet active />
-          <Bullet />
-        </Steps>
-      </Header>
+            <Steps>
+              <Bullet />
+              <Bullet active />
+            </Steps>
+          </Header>
 
-      <Title>Crie sua {'\n'}conta</Title>
-      <Subtitle>Faça seu cadastro de {'\n'}forma rápida e fácil</Subtitle>
+          <Title>Crie sua {'\n'}conta</Title>
+          <Subtitle>Faça seu cadastro de {'\n'}forma rápida e fácil</Subtitle>
 
-      <Form>
-        <FormTitle>2. Senha</FormTitle>
-        
-      </Form>
+          <Form>
+            <FormTitle>2. Senha</FormTitle>
 
-      <Button 
-        title="Próximo"
-      />
+            <PasswordInput 
+              iconName="lock"
+              placeholder="Senha"
+            />
+
+            <PasswordInput 
+              iconName="lock"
+              placeholder="Repetir Senha"
+            />
+          </Form>
+
+          <Button 
+            title="Cadastrar"
+            color={theme.colors.success}
+          />
         </Container>
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
