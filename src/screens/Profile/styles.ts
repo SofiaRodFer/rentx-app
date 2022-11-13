2712ce56324ed3f030b7ef8bc3b8cfa953bc846e
Sofia <sofiarodrigues45@gmail.com>
2022-11-13 17:18:00 -0300

    começando componente de troca entre dados e senha

--- src/screens/Profile/styles.ts
+++ src/screens/Profile/styles.ts
@@ -1,7 +1,12 @@
+import { TouchableOpacity } from 'react-native';
 import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 import { RFValue } from 'react-native-responsive-fontsize';
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
+
+interface OptionProps {
+  active: boolean;
+}
 
 export const Container = styled.View`
   flex: 1;
@@ -66,4 +71,37 @@ export const PhotoButton = styled(RectButton)`
   align-items: center;
 
   background-color: ${({ theme }) => theme.colors.main};
+`;
+
+
+export const Content = styled.View`
+  flex: 1;
+
+  padding: 0 24px;
+  margin-top: 122px;
+`;
+
+export const Options = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+
+  margin-bottom: 24px;
+
+  border-bottom-width: 1px;
+  border-bottom-color: ${({ theme }) => theme.colors.line};
+`;
+
+export const Option = styled(TouchableOpacity)<OptionProps>`
+  padding-bottom: 14px;
+
+  ${({ active }) => active && css`
+    border-bottom-width: 3px;
+    border-bottom-color: ${({ theme }) => theme.colors.main};
+  `}
+`;
+
+export const OptionTitle = styled.Text<OptionProps>`
+  font-family: ${({ theme, active }) => active ? theme.fonts.secondary_600 : theme.fonts.secondary_500};
+  font-size: ${RFValue(20)}px;
+  color: ${({ theme, active }) => active ? theme.colors.header : theme.colors.text_detail};
 `;
